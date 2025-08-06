755358d9d5f44a7b3fdc27bca7b7bddfcd456d87
helsaint <alexei.ramotar@gmail.com>
2025-08-05 21:57:33 -0400

    Reverted to dotenv instead of django-environ

--- config/settings.py
+++ config/settings.py
@@ -25,12 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-azg#-(o(756m5p-qy7t_tav&w!wvefi58u%73)5b1d&557lk9m"
+#SECRET_KEY = "django-insecure-azg#-(o(756m5p-qy7t_tav&w!wvefi58u%73)5b1d&557lk9m"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -97,6 +98,7 @@ DATABASES = {
 }
 '''
 
+
 DATABASES = {
     'default': dj_database_url.config(
         # The 'conn_max_age' argument is for old-style persistent connections.
@@ -116,6 +118,7 @@ DATABASES['default']['OPTIONS'] = {
         "max_lifetime": 1800,  # Ensure this is less than Heroku's 1-hour timeout
     }
 }
+
 
 
 
